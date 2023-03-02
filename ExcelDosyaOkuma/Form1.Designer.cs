64b86856e9d92f5b0c9ba1445c655ee21380509c
Dell <Dell@DESKTOP-2Q93AO7>
2023-03-02 17:40:43 +0300

    Add project files.

---
+++ ExcelDosyaOkuma/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace ExcelDosyaOkuma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.DosyaSec = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // DosyaSec
+            // 
+            this.DosyaSec.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.DosyaSec.BackColor = System.Drawing.Color.LightSlateGray;
+            this.DosyaSec.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.DosyaSec.Location = new System.Drawing.Point(103, 435);
+            this.DosyaSec.Margin = new System.Windows.Forms.Padding(8, 8, 8, 8);
+            this.DosyaSec.Name = "DosyaSec";
+            this.DosyaSec.Size = new System.Drawing.Size(143, 63);
+            this.DosyaSec.TabIndex = 0;
+            this.DosyaSec.Text = "Dosya Seç";
+            this.DosyaSec.UseVisualStyleBackColor = false;
+            this.DosyaSec.Click += new System.EventHandler(this.DosyaSec_Click_1);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.LightSlateGray;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-1, 74);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(8, 8, 8, 8);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(880, 345);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.LightSlateGray;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(651, 435);
+            this.button1.Margin = new System.Windows.Forms.Padding(8, 8, 8, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 63);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Kaydet";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-1, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(870, 72);
+            this.panel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Ravie", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(86, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(755, 59);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Excel Dosyalarını Düzenleyin...";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.ClientSize = new System.Drawing.Size(872, 512);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.DosyaSec);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(8, 8, 8, 8);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Excel Dosyası Okuma";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button DosyaSec;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ExcelDosyaOkuma/Form1.cs
@@ -0,0 +1,128 @@
+﻿
+using System;
+using System.Data;
+using System.Reflection;
+using System.Windows.Forms;
+using ExcelApp = Microsoft.Office.Interop.Excel;
+
+namespace ExcelDosyaOkuma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+
+        public DataTable ToDataTable(ExcelApp.Range range, int rows, int cols)
+        {
+            DataTable table = new DataTable();
+
+            for (int i = 1; i <= rows; i++)
+            {
+                if (i == 1)
+                { 
+                    for (int j = 1; j <= cols; j++)
+                    {
+                       
+                        if (range.Cells[i, j] != null && range.Cells[i, j].Value2 != null)
+                            table.Columns.Add(range.Cells[i, j].Value2.ToString());
+                        else
+                            table.Columns.Add(j.ToString() + ".Sütun");
+                    }
+                    continue;
+                }
+
+                var yeniSatir = table.NewRow();
+                for (int j = 1; j <= cols; j++)
+                {
+                   
+                    if (range.Cells[i, j] != null && range.Cells[i, j].Value2 != null)
+                        yeniSatir[j - 1] = range.Cells[i, j].Value2.ToString();
+                    else
+                        yeniSatir[j - 1] = String.Empty;
+                }
+                table.Rows.Add(yeniSatir);
+
+            }
+            return table;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ExcelApp.Application excelApp = new ExcelApp.Application();
+
+            excelApp.Visible = true;
+            object Missing = Type.Missing;
+            
+            ExcelApp.Workbook workbook = excelApp.Workbooks.Add(Missing);
+            ExcelApp.Worksheet sheet1 = (ExcelApp.Worksheet)workbook.Sheets[1];
+            int StartCol = 1;
+            int StartRow = 1;
+            for (int j = 0; j< dataGridView1.Columns.Count; j++)
+            {
+                ExcelApp.Range myRange = (ExcelApp.Range)sheet1.Cells[StartRow, StartCol + j];
+                myRange.Value2 = dataGridView1.Columns[j].HeaderText;
+            }
+            StartRow++;
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                for (int j = 0; j< dataGridView1.Columns.Count; j++)
+                {
+
+                    ExcelApp.Range myRange = (ExcelApp.Range)sheet1.Cells[StartRow + i, StartCol + j];
+                    myRange.Value2 = dataGridView1[j, i].Value == null ? "" : dataGridView1[j, i].Value;
+                    myRange.Select();
+
+
+                }
+            }
+            
+        }
+
+        private void DosyaSec_Click_1(object sender, EventArgs e)
+        {
+            string Yol;
+            string DosyaAdi;
+            DataTable dt;
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "Excel Dosyası | *.xls; *.xlsx; *.xlsm";
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                Yol = file.FileName;
+
+                DosyaAdi = file.SafeFileName;
+
+                ExcelApp.Application excelApp = new ExcelApp.Application();
+                if (excelApp == null)
+                {
+                    MessageBox.Show("Excel yüklü değil.");
+                    return;
+                }
+
+                ExcelApp.Workbook excelBook = excelApp.Workbooks.Open(Yol);
+
+                ExcelApp._Worksheet excelSheet = excelBook.Sheets[1];
+
+                ExcelApp.Range excelRange = excelSheet.UsedRange;
+
+                int satirSayisi = excelRange.Rows.Count;
+                int sutunSayisi = excelRange.Columns.Count;
+                dt = ToDataTable(excelRange, satirSayisi, sutunSayisi);
+
+                dataGridView1.DataSource = dt;
+                dataGridView1.Refresh();
+
+                excelApp.Quit();
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+
+            }
+            else
+            {
+                MessageBox.Show("Dosya Seçilemedi.");
+            }
+        }
+    }
+}
